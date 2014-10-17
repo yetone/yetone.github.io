9ef781dea9dd082af58f24c9fc42cd697d5c1fdf
yetone <i@yetone.net>
2014-10-17 12:30:03 +0800

    fix bug

--- js/app.js
+++ js/app.js
@@ -149,7 +149,7 @@ $(function() {
       if (typeof key === 'string') {
         fileCacheService.set(key, txt);
       }
-      acc.push('<a class="raw-url" href="' + file.raw_url + '">' + file.filename + '</a>\n```' + file.language.toLowerCase() + '\n' + txt + '\n```');
+      acc.push('<a target="_blank" class="raw-url" href="' + file.raw_url + '">' + file.filename + '</a>\n```' + file.language.toLowerCase() + '\n' + txt + '\n```');
       if (acc.length === fileNames.length) {
         cbk(acc);
       }
